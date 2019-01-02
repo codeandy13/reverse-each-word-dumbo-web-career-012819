14bf6c6d7143392737904bb4ca21e45965be256d
Andrew Kim <akim618@gmail.com>
2019-01-02 18:50:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word
   first_array = string.split(" ")
-  return array = []
-  first_array << string.split(" ")
+  return_array = []
+  first_array.each do |string|
+  return_array << string.split(" ")
 end
