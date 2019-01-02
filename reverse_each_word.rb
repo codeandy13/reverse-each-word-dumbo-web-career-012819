8b18e1a4b45639d1748f76b7cc4cb740fabfca4d
Andrew Kim <akim618@gmail.com>
2019-01-02 18:53:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   first_array = string.split(" ")
   return_array = []
   first_array.each do |string|
-  return_array << string.reverse(" ")
+  return_array << string.reverse
 end
   return_array.join(" ")
 end
